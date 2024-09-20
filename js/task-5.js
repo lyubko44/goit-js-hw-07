30f1eb506543ea3b29a3ce15ff1dec8318595a4b
Liubomyr Skorokhod <lyubko44@gmail.com>
2024-09-20 16:06:44 -0700

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body =  document.querySelector("body")
+const colorSpan =  document.querySelector('.color')
+const changeColorBtn = document.querySelector(".change-color");
+
+function changeColorForBody() {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+}
+
+changeColorBtn.addEventListener("click", changeColorForBody)
