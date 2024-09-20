e2069978f8c0ec632829bc85a6b4cbee5ae1ee36
Liubomyr Skorokhod <lyubko44@gmail.com>
2024-09-20 14:06:37 -0700

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form")
 
+form.addEventListener("submit", event => {
+    event.preventDefault()
+
+    const email = form.elements.email.value.trim()
+    const password = form.elements.password.value.trim()
+
+    if (email === "" || password === "") {
+        return console.log("All form fields must be filled in");
+    }
+
+    const formData = {
+        email,
+        password,
+    };
+
+    console.log(formData);
+
+    form.reset();
+});
